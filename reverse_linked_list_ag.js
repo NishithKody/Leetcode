8e986b0aefe714d536c73229bd3bd2a628e4e3a3
NishithKody <113385563+NishithKody@users.noreply.github.com>
2022-09-16 01:02:48 -0400

    Create reverse_linked_list_ag.js
    https://leetcode.com/problems/reverse-linked-list/submissions/

---
+++ reverse_linked_list_ag.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    
+    let cur = head;
+    let prev = null;
+    let next = null;
+    // null <-a  b -> c -> d 
+    // c     n
+    while(cur != null){
+        next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next
+    }
+    return prev
+};
